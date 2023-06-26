195857dcbd159ee581f022a9be58aae46d4da84a
Bricol <37510324+bricoli@users.noreply.github.com>
2023-06-26 11:31:39 +0200

    v4.x_2

--- install.sh
+++ install.sh
@@ -13,12 +13,10 @@ cp init_sql.sql /tmp/init_sql.sql -f
 apt -f -y install dialog mosquitto mosquitto-clients redis-server redis-tools ruby-redis postgresql postgresql-doc postgresql-doc-14 apt-transport-https dirmngr isag
 
 # 2. setup PostgreSQL databases and users
-sudo -u -i postgres psql -c "create role chirpstack_as with login password 'chirpstack';"
-sudo -u -i postgres psql -c "create role chirpstack_ns with login password 'chirpstack';"
-sudo -u -i postgres psql -c "create database chirpstack_as with owner chirpstack_as;"
-sudo -u -i postgres psql -c "create database chirpstack_ns with owner chirpstack_ns;"
-sudo -u -i postgres psql chirpstack_as -c "create extension pg_trgm;"
-sudo -u -i postgres psql chirpstack_as -c "create extension hstore;"
+sudo -u -i postgres psql -c "create role chirpstack with login password 'chirpstack';"
+sudo -u -i postgres psql -c "create database chirpstack with owner chirpstack;"
+sudo -u -i postgres psql chirpstack -c "create extension pg_trgm;"
+sudo -u -i postgres psql chirpstack -c "create extension hstore;"
 sudo -u -i postgres psql -U postgres -f /tmp/init_sql.sql
 rm -f /tmp/init_sql.sql
 
@@ -28,28 +26,31 @@ rm -f /tmp/init_sql.sql
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
 sudo apt-key export 6DBCCA00 | sudo gpg --dearmour -o /usr/share/keyrings/chirpstack.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/chirpstack.gpg] https://artifacts.chirpstack.io/packages/4.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
-apt update
-apt install chirpstack-network-server
-apt install chirpstack-gateway-bridge
-apt install chirpstack-application-server
+sudo apt update
+sudo apt install chirpstack
+sudo apt install chirpstack-gateway-bridge
 
 #4. configure lora
 # configure chirpstack Server
 #cp -f /etc/chirpstack-network-server/chirpstack-network-server.toml  /etc/chirpstack-network-server/chirpstack-network-server.toml_bak
-cp -rf ./chirpstack-network-server_conf/*  /etc/chirpstack-network-server/
-chown -R networkserver:networkserver /etc/chirpstack-network-server
+cp -rf ./chirpstack_conf/*  /etc/chirpstack/
+chown -R networkserver:networkserver /etc/chirpstack
 
 # configure chirpstack App Server
 #cp -f /etc/chirpstack-application-server/chirpstack-application-server.toml /etc/chirpstack-application-server/chirpstack-application-server.toml_bak
-cp -f ./chirpstack-application-server.toml /etc/chirpstack-application-server/chirpstack-application-server.toml
-chown -R appserver:appserver /etc/chirpstack-application-server
+#cp -f ./chirpstack-application-server.toml /etc/chirpstack-application-server/chirpstack-application-server.toml
+chown -R appserver:appserver /etc/chirpstack
 
-# start chirpstack-network-server
-systemctl restart chirpstack-network-server
-
-# start chirpstack-application-server
-systemctl restart chirpstack-application-server
+# start chirpstack
+sudo systemctl start chirpstack
+# start chirpstack on boot
+sudo systemctl enable chirpstack
 
 # start chirpstack-gateway-bridge
 systemctl restart chirpstack-gateway-bridge
+# start chirpstack-gateway-bridge on boot
+sudo systemctl enable chirpstack-gateway-bridge
 
+
+#Print the ChirpStack log-output:
+sudo journalctl -f -n 100 -u chirpstack

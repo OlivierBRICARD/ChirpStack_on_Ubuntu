19f331da5b343b6565ecf8cc7e9388dca9285cde
Bricol <37510324+bricoli@users.noreply.github.com>
2023-06-26 10:28:00 +0200

    Keyrings
    Changement de répertoire gpg

--- install.sh
+++ install.sh
@@ -10,23 +10,24 @@ cp init_sql.sql /tmp/init_sql.sql -f
 #apt list --upgradable
 
 # 1. install requirements
-apt -f -y install dialog mosquitto mosquitto-clients redis-server redis-tools postgresql apt-transport-https dirmngr
+apt -f -y install dialog mosquitto mosquitto-clients redis-server redis-tools ruby-redis postgresql postgresql-doc postgresql-doc-14 apt-transport-https dirmngr isag
 
 # 2. setup PostgreSQL databases and users
-sudo -u postgres psql -c "create role chirpstack_as with login password 'chirpstack';"
-sudo -u postgres psql -c "create role chirpstack_ns with login password 'chirpstack';"
-sudo -u postgres psql -c "create database chirpstack_as with owner chirpstack_as;"
-sudo -u postgres psql -c "create database chirpstack_ns with owner chirpstack_ns;"
-sudo -u postgres psql chirpstack_as -c "create extension pg_trgm;"
-sudo -u postgres psql chirpstack_as -c "create extension hstore;"
-sudo -u postgres psql -U postgres -f /tmp/init_sql.sql
+sudo -u -i postgres psql -c "create role chirpstack_as with login password 'chirpstack';"
+sudo -u -i postgres psql -c "create role chirpstack_ns with login password 'chirpstack';"
+sudo -u -i postgres psql -c "create database chirpstack_as with owner chirpstack_as;"
+sudo -u -i postgres psql -c "create database chirpstack_ns with owner chirpstack_ns;"
+sudo -u -i postgres psql chirpstack_as -c "create extension pg_trgm;"
+sudo -u -i postgres psql chirpstack_as -c "create extension hstore;"
+sudo -u -i postgres psql -U postgres -f /tmp/init_sql.sql
 rm -f /tmp/init_sql.sql
 
 #3. install ChirpStack packages
 #3.1 install https requirements
 #apt -f -y install apt-transport-https dirmngr
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
-echo "deb https://artifacts.chirpstack.io/packages/4.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
+sudo apt-key export 6DBCCA00 | sudo gpg --dearmour -o /usr/share/keyrings/chirpstack.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/chirpstack.gpg] https://artifacts.chirpstack.io/packages/4.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
 apt update
 apt install chirpstack-network-server
 apt install chirpstack-gateway-bridge

98142b2817136d25c504524c5ceaa765a84a90fd
Bricol <37510324+bricoli@users.noreply.github.com>
2023-06-26 09:52:30 +0200

    V4.x

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ cp init_sql.sql /tmp/init_sql.sql -f
 apt -f -y install dialog mosquitto mosquitto-clients redis-server redis-tools postgresql apt-transport-https dirmngr
 
 # 2. setup PostgreSQL databases and users
-sudo -u postgres psql -c "create role chirpstack_as with login password 'dbpassword';"
-sudo -u postgres psql -c "create role chirpstack_ns with login password 'dbpassword';"
+sudo -u postgres psql -c "create role chirpstack_as with login password 'chirpstack';"
+sudo -u postgres psql -c "create role chirpstack_ns with login password 'chirpstack';"
 sudo -u postgres psql -c "create database chirpstack_as with owner chirpstack_as;"
 sudo -u postgres psql -c "create database chirpstack_ns with owner chirpstack_ns;"
 sudo -u postgres psql chirpstack_as -c "create extension pg_trgm;"
@@ -26,22 +26,11 @@ rm -f /tmp/init_sql.sql
 #3.1 install https requirements
 #apt -f -y install apt-transport-https dirmngr
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
-echo "deb https://artifacts.chirpstack.io/packages/3.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
+echo "deb https://artifacts.chirpstack.io/packages/4.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
 apt update
 apt install chirpstack-network-server
 apt install chirpstack-gateway-bridge
 apt install chirpstack-application-server
-
-#rm *.deb -f
-#3.2 download chirpstack packages
-#wget https://artifacts.chirpstack.io/packages/3.x/deb/pool/main/c/chirpstack-application-server/chirpstack-application-server_3.5.1_linux_armv7.deb
-#wget https://artifacts.chirpstack.io/packages/3.x/deb/pool/main/c/chirpstack-gateway-bridge/chirpstack-gateway-bridge_3.4.1_linux_armv7.deb
-#wget https://artifacts.chirpstack.io/packages/3.x/deb/pool/main/c/chirpstack-network-server/chirpstack-network-server_3.6.0_linux_armv7.deb
-
-#3.3 install chirpstack packages
-#dpkg -i chirpstack-application-server_3.5.1_linux_armv7.deb
-#dpkg -i chirpstack-gateway-bridge_3.4.1_linux_armv7.deb
-#dpkg -i chirpstack-network-server_3.6.0_linux_armv7.deb
 
 #4. configure lora
 # configure chirpstack Server
